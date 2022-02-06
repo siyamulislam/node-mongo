c706b0c34a600518bc58f81a5397efbe4b1195f2
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2022-02-06 21:51:24 +0600

    server req res added

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-
+const users= ["Asad","Manik","Graps",'karim']
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! 2')
+});
+app.get('/fruit/banana',(req,res)=>{
+  res.send({fruit:'Banana',quantity:100,price:10});
 })
-
+app.get('/user/:id',(req,res)=>{
+  const id=req.params.id;
+  const name=users[id];
+  console.log(req.query);
+  res.send({id,name});
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })

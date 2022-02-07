fde58141f248ea0c9b3574b8f1db0d91399a007d
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2022-02-07 19:27:35 +0600

    api access from another cliend

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const cors= require('cors')
+app.use(cors());
 
 const users= ["Asad","Manik","Graps",'karim']
+//get
 app.get('/', (req, res) => {
   res.send('Hello World! 2')
 });
@@ -14,6 +17,11 @@ app.get('/user/:id',(req,res)=>{
   const name=users[id];
   console.log(req.query);
   res.send({id,name});
+})
+
+//post 
+app.post('/addUser',(res,req)=>{
+  console.log('post req sent');
 })
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
